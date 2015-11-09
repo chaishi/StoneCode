2a738cfa45e25835e8241511da8f3a11c78a1cb5
罗雪 <974383157@qq.com>
2015-11-09 20:41:39 +0800

    gulp使用

---
+++ gulp/gulp-readme.md
@@ -0,0 +1,25 @@
+#gulp使用说明
+
+##目录结构
+
+- build 
+	* css 
+	* js
+	* img
+- css
+- js
+- img
+- less
+- src
+- index.html
+- gulpfile.js
+
+> [1] build/ 用于存储编译（合并压缩）后的.css, .js, .png, .jpg等文件
+> [2] css/ 用于存储 less/ 文件夹下.less文件合并编译完成的.css文件
+> [3] js/ 用于存储有src/文件夹下的.js文件合并后的Js文件
+> [4] less/ 用于存储.less源文件
+> [5] src/ 用于存储.js源文件
+> [6] gulpfile.js  gulp配置文件
+
+
+
---
+++ gulp/gulpfile.js
@@ -0,0 +1,84 @@
+/*
+ * @description 引入插件
+ */
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var copy = require('gulp-copy');
+var tinypng = require('gulp-tinypng');
+var imagemin = require('gulp-imagemin');
+
+/*
+ * @description 注册任务
+ */
+gulp.task('clean', function() {
+	gulp.src(['build/css', 'build/js'])
+	.pipe(clean());
+});
+
+//带有原文件的压缩
+gulp.task('minify', function() {
+	gulp.src(['js/**/*.js', '!js/**/*-min.js'])
+	.pipe(minify())
+	.pipe(gulp.dest('build/js/'));
+});
+
+//不带原文件压缩
+gulp.task('uglify', function() {
+	gulp.src(['js/**/*.js', '!js/**/*-min.js'])
+	.pipe(uglify())
+	.pipe(gulp.dest('build/js/'));
+});
+
+//合并压缩js文件
+gulp.task('buildJS', ['clean'], function() {
+	gulp.src(['src/concat1.js', 'src/concat2.js'])
+	.pipe(concat('concat.js'))
+	.pipe(gulp.dest('js/'))
+	.pipe(uglify())
+	.pipe(gulp.dest('build/js'));
+	
+	gulp.src(['src/test1.js', 'src/test2.js'])
+	.pipe(concat('test.js'))
+	.pipe(gulp.dest('js/'))
+	.pipe(uglify())
+	.pipe(gulp.dest('build/js/'));
+});
+
+//合并编译压缩css文件
+gulp.task('buildCSS', function() {
+	gulp.src(['less/**/*.less', '!less/**/_*.less'])
+	.pipe(concat('myLess.less'))
+	.pipe(less())
+	.pipe(gulp.dest('css/'))
+	.pipe(minifyCSS())
+	.pipe(gulp.dest('build/css/'));
+});
+
+//该任务未实践成功
+gulp.task('copyCSS', function() {
+	gulp.src('css/**/*.css')
+	.pipe(copy())
+	.pipe(gulp.dest('build/css/'));
+});
+
+//压缩 png 和  jpg 格式的图片，压缩需要API-KEY，可从如下网址获得：https://tinypng.com/developers
+gulp.task('tinypng', function() {
+	gulp.src('img/**/*.*')
+	.pipe(tinypng('si8Xu_lmYT3CXqefZg555k1QnPDd28za'))//参数为API-KEY
+	.pipe(gulp.dest('build/img/'));
+});
+
+//压缩图片，压缩率没有tinypng高
+gulp.task('imagemin', function() {
+	gulp.src('img/**/*.*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('build/img/'));
+});
+
+gulp.task('default',['buildJS', 'buildCSS']);
+
